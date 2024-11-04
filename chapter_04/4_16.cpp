ea7114edc205db723000396b64d4defef6b95007
asilbek <englishisakovich@gmail.com>
2024-11-04 21:27:16 +0500

    4.16

---
+++ chapter_04/4_16.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+int main() {
+    int hours = 0;
+    float payPerHour = 0;
+    float salary = 0;
+
+    while (1) {
+        cout << "Enter worked hours: ";
+        cin >> hours;
+
+        if (hours == -1) {
+            break;
+        }
+
+        cout << "Enter hourly rate of the employee: ";
+        cin >> payPerHour;
+
+        if (hours <= 40) {
+            salary = hours * payPerHour;
+        } else {
+            salary = payPerHour * 40 + (hours - 40) * 1.5 * payPerHour;
+        }
+
+        cout << fixed << setprecision(2); 
+        cout << "Salary is $" << salary << endl;
+    }
+
+
+    return 0;
+}
