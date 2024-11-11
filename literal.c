8fa7ada9fa96026c4567b313e1c7a1ebda0ee556
asilbek <englishisakovich@gmail.com>
2024-11-11 14:24:29 +0500

    Did clang format

--- literal.c
+++ literal.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main () {
-	double d;
-	d = e2;
-	printf("%f", d);
-	return 0;
+int main() {
+    double d;
+    d = e2;
+    printf("%f", d);
+    return 0;
 }
--- specifier.c
+++ specifier.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-	unsigned int height = 12345552;
-	long long dist = 12355666LL;
-	long double weight = 45.34566672;
-	
-	printf("%u %lld %.4Lf", height, dist, weight);
+    unsigned int height = 12345552;
+    long long dist = 12355666LL;
+    long double weight = 45.34566672;
 
-	return 0;
+    printf("%u %lld %.4Lf", height, dist, weight);
+
+    return 0;
 }
