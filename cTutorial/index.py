fb9d93c11f0a342fb82dfa878bd87a3ac9d427d6
yunusov02 <126067756+yunusov02@users.noreply.github.com>
2024-12-05 20:27:46 +0500

    added Person class

--- cTutorial/index.py
+++ cTutorial/index.py
@@ -2,3 +2,14 @@ print("Hello World")
 
 for i in range(5):
   print(i)
+
+
+class Person:
+  def __init__(self, fname, lname):
+    self.fname = fname
+    self.lname = lname
+  
+  def get_full_name(self):
+    return self.name + " " + self.lname
+
+  
